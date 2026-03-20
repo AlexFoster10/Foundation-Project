310073aae589ef13cb2dad44a3d92ec9afd53fae
Alex Foster <wookie471@outlook.com>
2026-03-20 17:45:10 +0000

    plotting added to main

--- app/src/main.py
+++ app/src/main.py
@@ -6,7 +6,9 @@ sys.path.append(temp)
 from app.src import validate,  ingest,  processing,  output
 import pandas as pd
 import logging
+import matplotlib.pyplot as plt
 
+# Set up logging
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 def setup_logger(name, log_file, level=logging.INFO):
     handler = logging.FileHandler(log_file, mode="w")      
@@ -20,13 +22,27 @@ main_logger = setup_logger('main_logger', 'app/tests/logs/main.log')
 
 
 def main():
+
+    # Ingest, validate, process, and output data
     df =  ingest.ingest_csv("app/data/raw/marketData2.csv")
     df =  validate.clean_df(df)
+    pf = df.copy()
     df =  processing.daily_return(df)
     df =  processing.price_spread(df)
     #df = processing.simple_moving_average(df,5)
     df =  processing.volume_change(df)
     output.output_csv(df,"app/data/processed/processed_data.csv")
+
+
+    # Plotting graph
+    for ticker, group in df.groupby("ticker"):
+        group = group.sort_values("trade_date")
+        plt.plot(group["trade_date"], group["close"], label=ticker)
+    plt.legend(title="Ticker")
+    plt.title("Stock Prices by Ticker")
+    plt.ylabel("Close Price")
+    plt.xticks(rotation=20)
+    plt.savefig("app/data/plots/processed_data.png")
 
 
 if __name__ == "__main__":
